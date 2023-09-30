2fed16e64542141eb024a62fed160228b385bfbd
VicenzoMarramarco <vicenzomarramarco@gmail.com>
2023-09-30 01:42:11 -0300

    corrigindo a classe autorizacao

--- Autorizacao.java
+++ Autorizacao.java
@@ -1,10 +1,14 @@
-public class Autorizacao {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Autorizacao implements Comparable<Autorizacao> {
+    private static int codigoGerador = 1;
     private int codigo;
-    private static int codigoGerador=1;
     private Medico medico;
     private AreasAtuacao tipoDoExame;
     private String data;
     private Paciente paciente;
+
     public Autorizacao(Medico medico, AreasAtuacao tipoDoExame, String data, Paciente paciente) {
         this.medico = medico;
         this.tipoDoExame = tipoDoExame;
@@ -13,25 +17,37 @@ public class Autorizacao {
         codigo=codigoGerador;
         codigoGerador++;
     }
+
     public int getCodigo() {
         return codigo;
     }
+
     public Medico getMedico() {
         return medico;
     }
+
     public AreasAtuacao getTipoDoExame() {
         return tipoDoExame;
     }
+
     public String getData() {
         return data;
     }
+
     public Paciente getPaciente() {
         return paciente;
     }
+
     @Override
     public String toString() {
-        return "  Codigo:  " + codigo + "  Medico:  " + medico + "  TipoDoExame:  " + tipoDoExame + "  Data: " + data + "  Paciente: " + paciente ;
+        return "Codigo: " + codigo + ", Medico: " + medico + ", TipoDoExame: " + tipoDoExame + ", Data: " + data
+        + ", Paciente: " + paciente;
     }
 
-
+    @Override
+    public int compareTo(Autorizacao outro) {
+        LocalDate dateEsse = LocalDate.parse(data, DateTimeFormatter.ofPattern("dd/MM/yyyy")); 
+        LocalDate dataOutro = LocalDate.parse(outro.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return dateEsse.compareTo(dataOutro);
+    }
 }
